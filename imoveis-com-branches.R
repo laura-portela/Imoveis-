36069a82787030bfd6394749932109b09c6ba038
laura-portela <lauraportela@id.uff.br>
2026-04-15 16:25:55 -0300

    Corrigindo main para script esqueleto

--- imoveis-com-branches.R
+++ imoveis-com-branches.R
@@ -1,10 +1,9 @@
-# ETAPA 1: Leitura de dados
+#ETAPA 1: Leitura de dados
 dados <- read.csv2("C:/Users/laura/Downloads/imoveis.csv")
 
-# ETAPA 2: Gráficos
-class(dados$metragem)
-dados$metragem <- as.numeric(as.character(dados$metragem)) 
-hist(dados$metragem)
+#ETAPA 2: Gráficos
+#(será feito na branch graficos)
 
-# ETAPA 3: Medidas
-# (será feito na branch medidas)
+#ETAPA 3: Medidas
+#(será feito na branch medidas)
+
